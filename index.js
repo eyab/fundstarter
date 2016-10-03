8c2fb972f39cd42446d1d6bdd03e273791d78862
Ubuntu <ubuntu@ip-172-31-42-151.us-west-2.compute.internal>
2016-10-03 23:55:44 +0000

    part 1, b

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-var http = require('http')
-     var fs = require('fs')
-     var filename = 'index.html'
-    // console.log("App is running on localhost:8080");
-    
- http.createServer(function(req,response){
-	var buff = fs.readFileSync('index.html', "utf8")
-	response.writeHead(200, {"Content-type":"text/html"})
-	response.write(buff)
-	response.end()}).listen(8080);
+// needs node http modules 
+var http=require('http');
+var url=require('url');
+var fs=require('fs');
+
+// create new instance 
+http.createServer(function (req, res) {
+    // callback for the httpServer
+
+    var parse=url.parse(req.url,true);
+    var path=parse.pathname;
+    // replying to the browser
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+
+    fs.readFile('index.html', 'utf8',function (err, data) {
+if(err){
+            res.write('bad file');
+            throw err;
+            res.end();
+        }
+        if(data){
+            res.write(data.toString('utf8'));
+        // write something to browser 
+              res.write('<h3>Using readFile method:Read file in asynchronously (non-blocking)</h3>');
+              res.write(path);
+            res.end();
+        }
+    });
+}).listen(8080); // the server will listen on port 8080
+
 
